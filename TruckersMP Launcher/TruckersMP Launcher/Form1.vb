f873ab8c15fb03230e9046f78129c15aa4df326e
NarodGaming <arron.faulkner@gmail.com>
2017-04-26 21:55:51 +0100

    YART-0.0.0.2-260417

--- TruckersMP Launcher/TruckersMP Launcher/Form1.vb
+++ TruckersMP Launcher/TruckersMP Launcher/Form1.vb
@@ -184,7 +184,7 @@ Public Class Form1
         checkversion.Headers.Add("Expries", "-1")
         Dim versionresponse As String = checkversion.DownloadString("https://raw.githubusercontent.com/NarodGaming/narodtruckersmplauncher/master/Updates/currentver.txt?t=" + Date.Now.ToLocalTime) ' adds the date to prevent caching
 
-        If Not versionresponse = Application.ProductVersion + "-120417.Hotfix" Then ' if out of date
+        If Not versionresponse = Application.ProductVersion + "-260417" Then ' if out of date
             Dim wouldliketoupdate As MsgBoxResult = MsgBox("A new version is available, would you like to download it?", MsgBoxStyle.YesNo, "An update is available!") ' tells the user to update
             If wouldliketoupdate = MsgBoxResult.Yes Then
                 Process.Start("https://github.com/NarodGaming/narodtruckersmplauncher/releases") ' opens download page
--- TruckersMP Launcher/TruckersMP Launcher/SplashScreen.vb
+++ TruckersMP Launcher/TruckersMP Launcher/SplashScreen.vb
@@ -1,6 +1,6 @@
 ﻿Public Class SplashScreen
     Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        yart_version_label.Text = Application.ProductVersion + "-120417.Hotfix"
+        yart_version_label.Text = Application.ProductVersion + "-260417"
 
         show_splash_timer.Start()
     End Sub
